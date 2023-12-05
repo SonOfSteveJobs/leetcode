10a34a218adc12eea5b2490c9831f11108d06715
SonOfSteveJobs <evgenev2222@gmail.com>
2023-12-05 17:52:49 +0300

    94

---
+++ src/Trees/94.binary-tree-inorder-traversal.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=94 lang=typescript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    const result = [];
+    function traverse(root: TreeNode | null): number[] {
+        if (root) {
+            traverse(root.left);
+            result.push(root.val);
+            traverse(root.right);
+        }
+        return result;
+    }
+    return traverse(root);
+};
+// @lc code=end
+
