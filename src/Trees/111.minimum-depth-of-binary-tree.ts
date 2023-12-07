8547ac0f00531ca5b2771146a1235b3e06be07bc
SonOfSteveJobs <evgenev2222@gmail.com>
2023-12-07 16:53:41 +0300

    [111] Minimum Depth of Binary Tree

---
+++ src/Trees/111.minimum-depth-of-binary-tree.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=111 lang=typescript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function minDepth(root: TreeNode | null): number {
+    if (root === null) return 0
+
+    const left = minDepth(root.left) + 1
+    const right = minDepth(root.right) + 1
+
+    if (!root.left) return right
+    if (!root.right) return left
+
+    return Math.min(left, right)
+};
+// @lc code=end
+
